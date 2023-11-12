b9f7a9432628ff635ac36526e9254d4ace2833c8
realdump <a.majeed17@outlook.com>
2023-11-12 15:54:56 +0530

    updates

--- ATMapp/src/com/abmn/AtmOperationImpl.java
+++ ATMapp/src/com/abmn/AtmOperationImpl.java
@@ -16,18 +16,38 @@ public class AtmOperationImpl implements AtmOperationInterf {
 	@Override
 	public void withdrawAmount(double withdrawAmount) {
 		// TODO Auto-generated method stub
+		if(withdrawAmount%500==0) {
+            if (withdrawAmount <= atm.getBalance()) {
+                ministmt.put(withdrawAmount, " Amount Withdrawn");
+                System.out.println("Collect the Cash " + withdrawAmount);
+                atm.setBalance(atm.getBalance() - withdrawAmount);
+                viewBalance();
+            } else {
+                System.out.println("Insufficient Balance !!");
+            }
+        }
+        else {
+            System.out.println("Please enter the amount in multiples of 500");
+        }
 
 	}
 
 	@Override
 	public void depositAmount(double depositAmount) {
 		// TODO Auto-generated method stub
+		 ministmt.put(depositAmount," Amount Deposited");
+	     System.out.println(depositAmount+" Deposited Successfully !!");
+	     atm.setBalance(atm.getBalance()+depositAmount);
+	     viewBalance();
 
 	}
 
 	@Override
 	public void viewMiniStatement() {
 		// TODO Auto-generated method stub
+		for(Map.Entry<Double,String> m:ministmt.entrySet()){
+            System.out.println(m.getKey()+""+m.getValue());
+        }
 
 	}
 
